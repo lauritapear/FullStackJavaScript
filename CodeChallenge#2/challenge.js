1921f89511a9d81d5da175ea83aa2a88104a0489
LauraArtunduaga <Laura.Artunduaga@ge.com>
2016-05-24 15:45:07 -0400

    creating new div of class plannet

---
+++ CodeChallenge#2/challenge.js
@@ -0,0 +1,7 @@
+// Part 1: Create a new div of class "planet" and set its background color
+// to the color of your choice.  Then, append it to the body in the DOM.
+var new_row = document.createElement("div");
+new_row.setAttribute("class", "planet" );
+
+
+// Part 2: Create a new div of class "moon" and append it to the planet in the DOM.
