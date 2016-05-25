a2b8c3f06154cf2ce836f266f188a68aa72b0c21
lauritapear <laura.perea.artunduaga@gmail.com>
2016-05-25 09:30:58 -0400

    adding moon to the plannet

--- CodeChallenge#2/challenge.js
+++ CodeChallenge#2/challenge.js
@@ -1,13 +1,22 @@
 // Part 1: Create a new div of class "planet" and set its background color
 // to the color of your choice.  Then, append it to the body in the DOM.
-var new_row = document.createElement("div");
-new_row.setAttribute("class", "planet" );
+var thePlanet = document.createElement("div");
+thePlanet.setAttribute("class", "planet" );
 // Add some text
-new_row.appendChild( document.createTextNode("here is the planet") );
+thePlanet.appendChild( document.createTextNode("here is the planet") );
 // Change backgroundColor
-new_row.style.backgroundColor='red';
+thePlanet.style.backgroundColor='red';
 // Add it to the document body
-document.body.appendChild( new_row );
+document.body.appendChild( thePlanet );
 
+// Part 2: Create a new div of class "moon" and append it to the planet
+//in the DOM.
+var theMoon = document.createElement("div");
+theMoon.setAttribute("class", "moon" );
+// Add some text
+theMoon.appendChild( document.createTextNode("Moon") );
+// Change backgroundColor
+theMoon.style.backgroundColor='green';
 
-// Part 2: Create a new div of class "moon" and append it to the planet in the DOM.
+// Add moon to the planet
+thePlanet.appendChild(theMoon);
