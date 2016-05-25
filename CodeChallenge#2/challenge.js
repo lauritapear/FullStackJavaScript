1d1b8a5b7d85bbab762eb14cba4f87ef7496fcd8
lauritapear <laura.perea.artunduaga@gmail.com>
2016-05-25 09:30:45 -0400

    changing backgorund color and adding element to body

--- CodeChallenge#2/challenge.js
+++ CodeChallenge#2/challenge.js
@@ -2,6 +2,12 @@
 // to the color of your choice.  Then, append it to the body in the DOM.
 var new_row = document.createElement("div");
 new_row.setAttribute("class", "planet" );
+// Add some text
+new_row.appendChild( document.createTextNode("here is the planet") );
+// Change backgroundColor
+new_row.style.backgroundColor='red';
+// Add it to the document body
+document.body.appendChild( new_row );
 
 
 // Part 2: Create a new div of class "moon" and append it to the planet in the DOM.
