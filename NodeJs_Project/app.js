36f3aaddf5c79a954cba22f314f04ad8a39137a9
LauraArtunduaga <laura.artunduaga@ge.com>
2016-06-23 21:48:19 -0400

    Node js project to search for treehouse profiles

---
+++ NodeJs_Project/app.js
@@ -0,0 +1,17 @@
+var router = require("./router.js");
+//Problem: We need a simple way to look at a user's badge count and JavaScript point from a web browser
+//Solution: Use Node.js to perform the profile look ups and server our template via HTTP
+
+//Create a web server
+var http = require('http');
+http.createServer(function (request, response) {
+  router.home(request, response);
+  router.user(request, response);
+}).listen(3000);
+console.log('Server running at http://<workspace-url>/');
+
+
+
+
+
+
---
+++ NodeJs_Project/router.js
@@ -0,0 +1,87 @@
+var Profile = require("./profile.js");
+var renderer = require("./renderer.js");
+var querystring = require("querystring");
+
+var commonHeaders = {'Content-Type': 'text/html'};
+
+//Handle HTTP route GET / and POST / i.e. Home
+function home(request, response) {
+  //if url == "/" && GET
+  if(request.url === "/") {
+    if(request.method.toLowerCase() === "get") {
+      //show search
+      response.writeHead(200, commonHeaders);  
+      renderer.view("header", {}, response);
+      renderer.view("search", {}, response);
+      renderer.view("footer", {}, response);
+      response.end();
+    } else {
+      //if url == "/" && POST
+      
+      //get the post data from body
+      request.on("data", function(postBody) {
+        //extract the username
+        var query = querystring.parse(postBody.toString());
+        response.write(query.username);
+        response.end();
+        //redirect to /:username
+      });
+      
+    }
+  }
+  
+}
+
+//Handle HTTP route GET /:username i.e. /chalkers
+function user(request, response) {
+  //if url == "/...."
+  var username = request.url.replace("/", "");
+  if(username.length > 0) {
+    response.writeHead(200, commonHeaders);  
+    renderer.view("header", {}, response);    
+    
+    //get json from Treehouse
+    var studentProfile = new Profile(username);
+    //on "end"
+    studentProfile.on("end", function(profileJSON){
+      //show profile
+      
+      //Store the values which we need
+      var values = {
+        avatarUrl: profileJSON.gravatar_url, 
+        username: profileJSON.profile_name,
+        badges: profileJSON.badges.length,
+        javascriptPoints: profileJSON.points.JavaScript
+      }
+      //Simple response
+      renderer.view("profile", values, response);
+      renderer.view("footer", {}, response);
+      response.end();
+    });
+        
+    //on "error"
+    studentProfile.on("error", function(error){
+      //show error
+      renderer.view("error", {errorMessage: error.message}, response);
+      renderer.view("search", {}, response);
+      renderer.view("footer", {}, response);
+      response.end();
+    });
+      
+  }
+}
+
+module.exports.home = home;
+module.exports.user = user;
+
+
+
+
+
+
+
+
+
+
+
+
