c56e36968e623a04124b1ce66b90bc4bfcf75cd2
LauraArtunduaga <laura.artunduaga@ge.com>
2016-06-25 23:29:47 -0400

    adding HTMl to the response

--- Express_FirstApp/src/app.js
+++ Express_FirstApp/src/app.js
@@ -1,9 +1,11 @@
 'use strict';
 
 var express = require('express');
+
 var app = express();
 
 app.get('/', function(request, response){
-  response.send('Hey, Laura');
+	response.send("<h1> Hola Laura!</h1>");
 });
+
 app.listen(3000);
