cd157b877768cfe012963e214b0a31391c8462c7
LauraArtunduaga <laura.artunduaga@ge.com>
2016-06-27 23:02:54 -0400

    first steps with jade

---
+++ Jade_project/src/app.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var express = require('express'),
+	  posts = require('./mock/posts.json');
+
+var app = express();
+
+app.get('/', function(req, res){
+	res.send("<h1>I am loving Treehouse!</h1>");
+});
+
+app.get('/blog/:title?', function(req, res){ 
+	var title = req.params.title;
+	if (title === undefined) {
+		res.status(503);
+		res.send("This page is under construction!")
+	} else {
+		var post = posts[title];
+		res.send(post);
+	}
+});
+
+app.listen(3000, function(){
+	console.log("The frontend server is running on port 3000!")
+});
