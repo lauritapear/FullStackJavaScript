7917183af36659b257e4ca8cefec66a1a5eab602
LauraArtunduaga <laura.artunduaga@ge.com>
2016-07-04 19:00:28 -0400

    working with express

---
+++ Express/src/app.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var express = require('express'),
+	  posts = require('./mock/posts.json');
+
+var app = express();
+
+app.use('/static', express.static(__dirname + '/public'))
+
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/templates');
+
+app.get('/', function(req, res){
+	res.render('index');
+});
+
+app.get('/blog/:title?', function(req, res){
+	var title = req.params.title;
+	if (title === undefined) {
+		res.status(503);
+		res.send("This page is under construction!");
+	} else {
+		var post = posts[title] || {};
+		res.render('post', { post: post});
+	}
+});
+
+app.listen(3000, function() {
+	console.log("The frontend server is running on port 3000!");
+});
