00fd229737ce100efe338b4ebb55e6369f0bcfb7
LauraArtunduaga <laura.artunduaga@ge.com>
2016-07-08 08:47:28 -0400

    angular

--- AngularBasic/scripts/app.js
+++ AngularBasic/scripts/app.js
@@ -3,5 +3,19 @@ angular.module("todoListApp", [])
   $scope.helloWorld = function(){
     console.log ("Hello there! This is the helloworld controller function int he main controller");
 
-  }
+  };
 })
+
+.controller('coolCtrl', function($scope){
+  $scope.whoAmI = function(){
+    console.log ("Hello there! This is the coolCtrl function!");
+  };
+
+  $scope.helloWorld = function(){
+    console.log ("This is not mainCtrl!");
+  };
+})
+.controller('imASibiling', function($scope){
+  $scope.foobar = 1234;
+  //do other stuff 
+});
