3c001624d627d98ae42287a7acd72b4a6fd5ca64
LauraArtunduaga <laura.artunduaga@ge.com>
2016-07-08 23:10:52 -0400

    todo list app almost ready

--- AngularBasic/scripts/app.js
+++ AngularBasic/scripts/app.js
@@ -1,21 +1,24 @@
 angular.module("todoListApp", [])
 .controller('mainCtrl', function($scope){
-  $scope.helloWorld = function(){
-    console.log ("Hello there! This is the helloworld controller function int he main controller");
+  $scope.learningNgChange = function(){
+    console.log ("An input changed!");
 
   };
+// });
+$scope.todos = [
+  {"name": "Clean the house"},
+  {"name": "Water the dog"},
+  {"name": "Feed the lawn"},
+  {"name": "Pay the bills"},
+  {"name": "Run"},
+  {"name": "Swim"}
+]
 })
-
-.controller('coolCtrl', function($scope){
-  $scope.whoAmI = function(){
-    console.log ("Hello there! This is the coolCtrl function!");
-  };
-
-  $scope.helloWorld = function(){
-    console.log ("This is not mainCtrl!");
-  };
-})
-.controller('imASibiling', function($scope){
-  $scope.foobar = 1234;
-  //do other stuff 
-});
+// .controller('coolCtrl', function($scope){
+//   $scope.whoAmI = function(){
+//     console.log ("Hello there! This is the coolCtrl function!");
+//   };
+//
+//   $scope.helloWorld = function(){
+//     console.log ("This is not mainCtrl!");
+//   }
