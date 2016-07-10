4695618c96a9c75cdd342c54f4a39ead67201bdd
LauraArtunduaga <laura.artunduaga@ge.com>
2016-07-10 11:39:34 -0400

    adding services and controllers folders to organize scripts

---
+++ AngularBasic/scripts/controllers/main.js
@@ -0,0 +1,26 @@
+'use strict';
+angular.module('todoListApp')
+.controller("mainCtrl", function($scope, dataService) {
+        $scope.addTodo = function () {
+          var todo = {name:"This is a new todo"};
+          $scope.todos.push(todo);
+  };
+
+        $scope.helloConsole = dataService.helloConsole;
+
+        $scope.learningNgChange = function() {
+            console.log("Value Changed!!");
+        };
+
+        dataService.getTodos(function(response) {
+            console.log(response.data);
+            $scope.todos = response.data;
+        });
+
+        $scope.deleteTodo = function(todo, index) {
+            dataService.deleteTodo;
+            $scope.todos.splice(index, 1);
+        };
+
+        $scope.saveTodo = dataService.saveTodo;
+    })
