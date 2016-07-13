35819e1a111ff2272317dce2dfca4c567604aed0
LauraArtunduaga <laura.artunduaga@ge.com>
2016-07-12 22:03:04 -0400

    MEAN

---
+++ mean-todo/app/scripts/controllers/todo.js
@@ -0,0 +1,21 @@
+'use strict';
+
+function TodoCtrl ($scope, dataService) {
+
+  $scope.deleteTodo = function(todo, index) {
+    $scope.todos.splice(index, 1);
+    dataService.deleteTodo(todo);
+  };
+
+  $scope.saveTodos = function() {
+    var filteredTodos = $scope.todos.filter(function(todo){
+      if(todo.edited) {
+        return todo
+      };
+    })
+    dataService.saveTodos(filteredTodos);
+  };
+
+}
+
+module.exports = TodoCtrl;
